5927e9e1d78e7a0f1da167a409b746728b5b650f
ozde pilli <o.pilli@student.rug.nl>
2025-05-11 10:24:00 +0200

    .ply files added

--- project_name/data/visualize.py
+++ project_name/data/visualize.py
@@ -1,8 +1,11 @@
 import os
 import pyminiply
 
-models_dir = r"C:\Users\ozdep\Documents\aml\Applied-ML-Group8\project_name\models"
-model_file = os.path.join(models_dir, "obj_01.ply")
+here = os.path.dirname(__file__)
+project_root = os.path.abspath(os.path.join(here, os.pardir))
+models_dir   = os.path.join(project_root, "models")
+model_file   = os.path.join(models_dir, "obj_01.ply")
+
 
 mesh = pyminiply.read_as_mesh(model_file)
 mesh.plot()
